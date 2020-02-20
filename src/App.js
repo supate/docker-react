7152a739331b69874d3198b09d35a8ac812433af
sunipate <sunil.patel@oracle.com>
2020-02-20 08:04:20 +0530

    updated message and test case

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          How Are you react
         </a>
       </header>
     </div>
